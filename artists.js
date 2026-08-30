f49a0f1b620a05e53ff85de8b26607b33c8bafa4
Musky Demon <48533449+Zelrog@users.noreply.github.com>
2026-08-31 01:09:42 +0200

    Create artists.js

---
+++ artists.js
@@ -0,0 +1,22 @@
+const artists = [
+    {
+        name: "Chronobyte",
+        aliases: ["oekanuki"],
+        imageExt: "png",
+        socials: {
+            fa: "https://www.furaffinity.net/user/~chronobyte",
+            bluesky: "https://bsky.app/profile/oekanuki.bsky.social"
+        }
+    },
+    {
+        name: "ArtistTwo",
+        aliases: ["Test1", "Test2"],
+        imageExt: "png", 
+        socials: {
+            fa: "https://furaffinity.net/user/artisttwo",
+            e621: "https://e621.net/posts?tags=artisttwo",
+            twitter: "https://twitter.com/artisttwo",
+            pixiv: "https://pixiv.net/users/123456"
+        }
+    }
+];
