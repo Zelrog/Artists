95f4e61945021eca08f37c171738cbd1732e7da5
Musky Demon <48533449+Zelrog@users.noreply.github.com>
2026-08-31 15:17:25 +0200

    Update artists.js

--- artists.js
+++ artists.js
@@ -1,22 +1,260 @@
 const artists = [
     {
         name: "Chronobyte",
-        aliases: ["oekanuki"],
+        aliases: ["Oekanuki"],
         imageExt: "png",
         socials: {
             fa: "https://www.furaffinity.net/user/~chronobyte",
-            bluesky: "https://bsky.app/profile/oekanuki.bsky.social"
+            bluesky: "https://bsky.app/profile/oekanuki.bsky.social",
+            e621: "https://e621.net/artists/124125"
         }
     },
     {
-        name: "ArtistTwo",
-        aliases: ["Test1", "Test2"],
+        name: "Spex",
+        aliases: ["Spectral-Bat", "Spectral-Rat"],
         imageExt: "png", 
         socials: {
-            fa: "https://furaffinity.net/user/artisttwo",
-            e621: "https://e621.net/posts?tags=artisttwo",
-            twitter: "https://twitter.com/artisttwo",
-            pixiv: "https://pixiv.net/users/123456"
+            e621: "https://e621.net/artists/11112"
+        }
+    },
+    {
+        name: "Galrock",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/26441"
+        }
+    },
+    {
+        name: "Danero",
+        aliases: ["Ilmandi"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/38954"
+        }
+    },
+    {
+        name: "Garn",
+        aliases: ["Garnable"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/102427"
+        }
+    },
+    {
+        name: "Demon Rat",
+        aliases: ["Stupid Rat"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/84318"
+        }
+    },
+    {
+        name: "Borges",
+        aliases: ["Aoba"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/124126"
+        }
+    },
+    {
+        name: "x6udpngx",
+        aliases: ["pngx"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/17515"
+        }
+    },
+    {
+        name: "contort desire",
+        aliases: ["ftdire"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/87531"
+        }
+    },
+    {
+        name: "SlugsDog",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/19265"
+        }
+    },
+    {
+        name: "Hoggan",
+        aliases: ["Exoticd", "Barnyardhog "],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/104189"
+        }
+    },
+    {
+        name: "Kotori",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/4580"
+        }
+    },
+    {
+        name: "Cywolf49",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/64817"
+        }
+    },
+    {
+        name: "Dramamine",
+        aliases: ["NightPhysics", "GorillaDrama"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/829"
+        }
+    },
+    {
+        name: "HiddenRaptor",
+        aliases: ["AnthroRaptor"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/55773"
+        }
+    },
+    {
+        name: "Razzper",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/58183"
+        }
+    },
+    {
+        name: "RuffusBleu",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/79301"
+        }
+    },
+    {
+        name: "PoweredByGif",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/85673"
+        }
+    },
+    {
+        name: "AlexxFur2",
+        aliases: ["quejumbrosoalex", "alexxelquejumbroso"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/91940"
+        }
+    },
+    {
+        name: "Captain Nikko",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/11819"
+        }
+    },
+    {
+        name: "BowserBoy101",
+        aliases: ["SobekDraws"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/20115"
+        }
+    },
+    {
+        name: "PataDePerro",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/93597"
+        }
+    },
+    {
+        name: "Mestiso",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/32685"
+        }
+    },
+    {
+        name: "Ozadene",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/124127"
+        }
+    },
+    {
+        name: "Memburu",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/29108"
+        }
+    },
+    {
+        name: "Orf",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/25879"
+        }
+    },
+    {
+        name: "Lower",
+        aliases: ["LowerKuo"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/32351"
+        }
+    },
+    {
+        name: "YeenStank",
+        aliases: ["Protheorem"],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/47583"
+        }
+    },
+    {
+        name: "CorvidTree",
+        aliases: ["", ""],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/96992"
+        }
+    },
+    {
+        name: "Andrefil360",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/59896"
+        }
+    },
+    {
+        name: "Nanoff",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/31940"
+        }
+    },
+    {
+        name: "SpikySketches",
+        aliases: [],
+        imageExt: "png", 
+        socials: {
+            e621: "https://e621.net/artists/57174"
         }
     }
 ];
